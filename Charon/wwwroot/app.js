09cc689279b99f605d4499ed296cb26e8c3a698d
Milosz Dura <milosz.md@wp.pl>
2017-08-23 12:24:00 +0200

    removing unused usings, removing obj forlder from vc

---
+++ Charon/wwwroot/app.js
@@ -0,0 +1,3 @@
+alert('hello world!');
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIldlYi9qcy9tYWluLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLEtBQUssQ0FBQyxjQUFjLENBQUMsQ0FBQyIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyJhbGVydCgnaGVsbG8gd29ybGQhJyk7Il19
